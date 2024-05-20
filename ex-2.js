3625dd857301368aaeb1263b09ba44b3ddf4af17
Apirak Fakin <apirakfakin@gmail.com>
2024-05-20 13:38:11 +0700

    feat: add ex-2

--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,7 @@
 let words = ["apple", "cat", "dog", "elephant"];
 
 function get5CharWords(words) {
-  // Start coding here
+  return words.filter((word)=> word.length >= 5)
 }
 
 const result = get5CharWords(words);
